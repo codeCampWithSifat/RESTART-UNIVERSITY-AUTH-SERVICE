cb74be26d6ee859a7a51706d61c01126666e10bd
Sayed Hossain Sifat <sayedhossainsifat300@gmail.com>
2024-03-20 00:37:38 +0600

    faculty route controller and service part are [done]

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -85,7 +85,7 @@ const getAllSemesters = async (
     .sort(sortConditions)
     .skip(skip)
     .limit(limit);
-  const total = await AcademicSemester.countDocuments();
+  const total = await AcademicSemester.countDocuments(whereConditons);
 
   return {
     meta: {
